b6e6a40e30d7a016e853fe13fbf78c9d27e576c8
yogesh <yogesh@bhawsar.dev>
2023-01-21 23:45:18 +0530

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Yogesh Bhawsar on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
